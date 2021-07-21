3e8d7648a32d489ac219a5cc992e4654ab5f827d
Malebestia <malebestia@yahoo.it>
2021-07-21 23:41:59 +0200

    .

--- Resources/views/errors/403.blade.php
+++ Resources/views/errors/403.blade.php
@@ -1,4 +1,4 @@
-@extends('errors::minimal')
+@extends('pub_theme::errors.illustrated-layout')
 
 @section('title', __('Forbidden'))
 @section('code', '403')
---
+++ Resources/views/shops/index.blade.php
@@ -0,0 +1,20 @@
+@extends('pub_theme::layouts.app',['sidebar'=>1])
+@section('content')
+	@foreach($rows  as $key=>$row)
+		<article class="docs-article" id="section-1">
+		    <header class="docs-header">
+			    <h1 class="docs-heading">
+			    	<a href="{{ Panel::get($row)->url() }}">{{ $row->title }}</a> <span class="docs-time">Last updated: {{ $row->updated_at }}</span>
+			    </h1>
+			    <section class="docs-intro">
+				    <p>{{ $row->subtitle }}</p>
+				</section><!--//docs-intro-->
+			</header>
+
+		</article>
+
+	@endforeach
+	{{--  
+    @include($_layout->view_default.'.test.content')
+    --}}
+@endsection
