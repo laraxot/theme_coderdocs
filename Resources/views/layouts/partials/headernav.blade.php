ff7f68558564342223347086e4312ca269e1bf3e
Malebestia <malebestia@yahoo.it>
2021-08-23 20:18:31 +0200

    .

--- Resources/views/layouts/partials/headernav.blade.php
+++ Resources/views/layouts/partials/headernav.blade.php
@@ -36,7 +36,7 @@
 						--}}
 
 
-					<form method="get">
+					<form method="get" action="{{ route('container0.index',['lang'=>$lang,'container0'=>'articles']) }}">
 						<div class="input-group input-group-sm">
 							<input type="search" name="q" id="form_search" value="{{ Request::input('q') }}" class="form-control" placeholder="Search" aria-label="Search">
 							<div class="input-group-append">
