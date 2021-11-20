382c043daf7166a6183e5032f263265644f4340b
Malebestia <malebestia@yahoo.it>
2021-11-20 12:24:21 +0100

    .

--- Resources/views/layouts/default/show.blade.php
+++ Resources/views/layouts/default/show.blade.php
@@ -1,33 +1,34 @@
 @extends('pub_theme::layouts.app')
 @section('content')
-@php
-    if(!is_object($row)){
-        dddx(['row'=>$row]);
+    @php
+    if (!is_object($row)) {
+        dddx(['row' => $row]);
     }
     //ddd(get_class($row)); //Modules\Blog\Models\Article
     //ddd($row->ratings);
     //ddd($row->ratingObjectives);
-@endphp
-  <article class="docs-article" id="section-1">
-	<header class="docs-header">
-		<h1 class="docs-heading">{{ $row->title }} <span class="docs-time">Last updated: {{ $row->updated_at }}</span></h1>
-        <p style="text-color:black;">
-            {{--
-            {!! Panel::get($row)->btnItemAction('rate') !!}
-            --}}
-        </p>
+    @endphp
+    <article class="docs-article" id="section-1">
+        <header class="docs-header">
+            <h1 class="docs-heading">{{ $row->title }} <span class="docs-time">Last updated:
+                    {{ $row->updated_at }}</span></h1>
+            <p style="text-color:black;">
+                {{-- {!! Panel::get($row)->btnItemAction('rate') !!} --}}
+            </p>
 
-		<section class="docs-intro">
-			<p>{{ $row->subtitle }}</p>
-		</section>
-		{!! $row->txt !!}
-	</header>
-	@foreach($row->sons as $son)
-	<section class="docs-section" id="item-1-1">
-		<h2 class="section-heading">{{ $son->title }}</h2>
-		{!! $son->txt !!}
-	</section>
-	@endforeach
-</article>
+            <section class="docs-intro">
+                <p>{{ $row->subtitle }}</p>
+            </section>
+            {!! $row->txt !!}
+        </header>
+        @if (method_exists($row, 'son'))
+            @foreach ($row->sons as $son)
+                <section class="docs-section" id="item-1-1">
+                    <h2 class="section-heading">{{ $son->title }}</h2>
+                    {!! $son->txt !!}
+                </section>
+            @endforeach
+        @endif
+    </article>
 
 @endsection
